03782b1f6a20ce29e36cae17322aadbdcaf9bb4e
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-27 13:23:53 -0300

    feat: add definition and interfaces for UserService and UserRepository, add User creation method

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,4 +1,4 @@
-package domain
+package internal
 
 import "time"
 
@@ -8,4 +8,38 @@ type User struct {
 	Email     string
 	CPF       string
 	BirthDate time.Time
+}
+
+type IUserRepository interface {
+	Save(*User) (User, error)
+}
+
+type IUserService interface {
+	Create(name string, email string, CPF string, birthDate string) (*User, error)
+}
+
+type UserService struct {
+	userRepository IUserRepository
+}
+
+func (userService UserService) Create(name string, email string, CPF string, birthDate string) (*User, error) {
+	birthDateTime, err := time.Parse("2006-01-02", birthDate)
+	if err != nil {
+		return nil, err
+	}
+
+	user := User{
+		ID:        "1",
+		Name:      name,
+		Email:     email,
+		CPF:       CPF,
+		BirthDate: birthDateTime,
+	}
+
+	createdUser, err := userService.userRepository.Save(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &createdUser, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 func main() {
-	log.Println("Started")
+	teste, err := time.Parse("2006-01-02", "2024-08-27")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println(teste)
 }
