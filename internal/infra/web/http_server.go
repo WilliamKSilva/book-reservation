47b5abb502bbd68953b96b02f64dc37331d44d34
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-30 18:10:23 -0300

    feat: add Swagger to deal with API documentation

--- internal/infra/web/http_server.go
+++ internal/infra/web/http_server.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 	"os"
 
+	_ "github.com/WilliamKSilva/book-reservation/docs"
 	"github.com/WilliamKSilva/book-reservation/internal/infra/web/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func registerUserRoutes(r *chi.Mux, userHandler handlers.IUserHandler) {
@@ -18,12 +20,29 @@ func registerUserRoutes(r *chi.Mux, userHandler handlers.IUserHandler) {
 	})
 }
 
+// @title Book Reservation API
+// @version 1.0
+// @description This is the API for the online Book Reservation service.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host book-reservation.swagger.io
+// @BasePath /v2
 func StartListening(port int, dbConn *pgx.Conn) {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	userHandler := handlers.NewUserHandler(dbConn)
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
+	))
 
+	userHandler := handlers.NewUserHandler(dbConn)
 	registerUserRoutes(r, userHandler)
 
 	log.Printf("Listening at %d", port)
