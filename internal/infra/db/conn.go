44df59f1d3ad7fd9c49bd7fe8c457a170fc3130a
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-09-04 19:04:04 -0300

    test: add test for PostgresUserRepository save method

--- internal/infra/db/conn.go
+++ internal/infra/db/conn.go
@@ -3,13 +3,53 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
-func Connect() *pgxpool.Pool {
-	// The *pgx.Conn returned by pgx.Connect() represents a single connection and is not concurrency safe.
+func ConnectTestDatabase() (context.Context, *pgxpool.Pool) {
+	ctx := context.Background()
+	err := godotenv.Load("../../../.env")
+	if err != nil {
+		log.Printf("Error loading .env: %s", err.Error())
+		os.Exit(1)
+	}
+
+	dbURL := os.Getenv("DATABASE_TEST_URL")
+	poolConfig, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		log.Printf("Unable to parse database URL: %s", err.Error())
+		os.Exit(1)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		log.Printf("Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		fmt.Printf("Error trying to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	return ctx, pool
+}
+
+func ResetDatabaseState(ctx context.Context, conn *pgxpool.Pool) error {
+	_, err := conn.Exec(ctx, `
+		TRUNCATE TABLE users_books_reservations CASCADE;
+		TRUNCATE TABLE users CASCADE;
+		TRUNCATE TABLE books CASCADE;
+	`)
+
+	return err
+}
+
+func Connect(ctx context.Context) *pgxpool.Pool {
 	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
 
 	if err != nil {
@@ -17,7 +57,7 @@ func Connect() *pgxpool.Pool {
 		os.Exit(1)
 	}
 
-	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
---
+++ internal/infra/db/postgres_repository_test.go
@@ -0,0 +1,57 @@
+package db
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/WilliamKSilva/book-reservation/internal/app/user"
+)
+
+// it should save a User entity in the database
+func mockUser() (user.User, error) {
+	birthDateTime, err := time.Parse("2006-01-02", "2024-08-13")
+	if err != nil {
+		return user.User{}, err
+	}
+	return user.User{
+		ID:        "ff508158-8da7-4840-b891-38c240f9aee1",
+		Name:      "johndoe",
+		Email:     "johndoe@teste.com",
+		CPF:       "12312312323",
+		Password:  "teste1234",
+		BirthDate: birthDateTime,
+	}, err
+}
+
+func TestPostgresUserRepositorySave(t *testing.T) {
+	ctx, conn := ConnectTestDatabase()
+	defer conn.Close()
+
+	userRepository := PostgresUserRepository{
+		Conn: conn,
+		Ctx:  ctx,
+	}
+
+	mockedUser, err := mockUser()
+	if err != nil {
+		t.Error("Error mocking User", err)
+		return
+	}
+
+	u, err := userRepository.Save(mockedUser)
+	if err != nil {
+		t.Errorf("Error trying to save User: %s", err)
+		return
+	}
+
+	if !reflect.DeepEqual(mockedUser, u) {
+		t.Errorf("Saved User does not match")
+		return
+	}
+
+	err = ResetDatabaseState(ctx, conn)
+	if err != nil {
+		t.Errorf("Error trying to reset database state: %s", err)
+	}
+}
