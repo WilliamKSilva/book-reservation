d7fad676456f296b03fc10f6bbeeef7657d21472
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-09-05 14:49:08 -0300

    test: add a test suit to run each method of PostgresRepository, instead of writing a Test function for each method

--- internal/infra/db/postgres_repository_test.go
+++ internal/infra/db/postgres_repository_test.go
@@ -24,34 +24,36 @@ func mockUser() (user.User, error) {
 	}, err
 }
 
-func TestPostgresUserRepositorySave(t *testing.T) {
+func TestPostgresUserRepository(t *testing.T) {
 	ctx, conn := ConnectTestDatabase()
 	defer conn.Close()
 
-	userRepository := PostgresUserRepository{
-		Conn: conn,
-		Ctx:  ctx,
-	}
+	t.Run("save user in the database", func(t *testing.T) {
+		userRepository := PostgresUserRepository{
+			Conn: conn,
+			Ctx:  ctx,
+		}
 
-	mockedUser, err := mockUser()
-	if err != nil {
-		t.Error("Error mocking User", err)
-		return
-	}
+		mockedUser, err := mockUser()
+		if err != nil {
+			t.Error("Error mocking User", err)
+			return
+		}
 
-	u, err := userRepository.Save(mockedUser)
-	if err != nil {
-		t.Errorf("Error trying to save User: %s", err)
-		return
-	}
+		u, err := userRepository.Save(mockedUser)
+		if err != nil {
+			t.Errorf("Error trying to save User: %s", err)
+			return
+		}
 
-	if !reflect.DeepEqual(mockedUser, u) {
-		t.Errorf("Saved User does not match")
-		return
-	}
+		if !reflect.DeepEqual(mockedUser, u) {
+			t.Errorf("Saved User does not match")
+			return
+		}
 
-	err = ResetDatabaseState(ctx, conn)
-	if err != nil {
-		t.Errorf("Error trying to reset database state: %s", err)
-	}
+		err = ResetDatabaseState(ctx, conn)
+		if err != nil {
+			t.Errorf("Error trying to reset database state: %s", err)
+		}
+	})
 }
