74b840971f70da0f795961b0259ac6c919a0859b
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-09-11 09:40:50 -0300

    test: ensure AuthService Login method return UserNotFoundError in case UserService FindByEmail method dont find an User

--- internal/services/tests/auth_service_test.go
+++ internal/services/tests/auth_service_test.go
@@ -1,6 +1,7 @@
 package services_tests
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 
@@ -9,18 +10,48 @@ import (
 )
 
 func TestAuthServiceLogin(t *testing.T) {
-	authService := services.AuthService{
-		JwtService:  NewMockedJwtServiceSuccess(),
-		UserService: NewMockedUserServiceSuccess(),
-	}
-
 	t.Run("should return an empty LoginResponseDTO struct and an error if UserService FindByEmail fails", func(t *testing.T) {
-		authService.UserService = NewMockedUserServiceFailure()
+		userRepository := NewMockedUserRepositoryFailure()
+		uuidGenerator := NewMockedUuidService()
+		userService := services.NewUserService(userRepository, uuidGenerator)
+
+		authService := services.AuthService{
+			JwtService:  NewMockedJwtServiceSuccess(),
+			UserService: userService,
+		}
 
 		res, err := authService.Login("teste", "teste12")
 
 		if err == nil {
 			t.Error("Expected err, got nil")
+		}
+
+		expected := DTOs.LoginResponseDTO{}
+
+		if !reflect.DeepEqual(expected, res) {
+			t.Error("Expected empty LoginResponseDTO struct, got populated struct")
+		}
+	})
+
+	t.Run("should return an empty LoginResponseDTO struct and an user not found error if UserService FindByEmail don't find an user", func(t *testing.T) {
+		userRepository := NewMockedUserRepositorySuccessFindByEmailNotFound()
+		uuidGenerator := NewMockedUuidService()
+		userService := services.NewUserService(userRepository, uuidGenerator)
+
+		authService := services.AuthService{
+			JwtService:  NewMockedJwtServiceSuccess(),
+			UserService: userService,
+		}
+
+		res, err := authService.Login("teste", "teste12")
+
+		if err == nil {
+			t.Error("Expected err, got nil")
+		}
+
+		var notFoundErr *services.UserNotFoundError
+		if !errors.As(err, &notFoundErr) {
+			t.Errorf("Expected error of type UserNotFoundError, but got %s", err.Error())
 		}
 
 		expected := DTOs.LoginResponseDTO{}
--- internal/services/tests/services_mocks.go
+++ internal/services/tests/services_mocks.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/WilliamKSilva/book-reservation/internal/domain"
 	repositories_tests "github.com/WilliamKSilva/book-reservation/internal/infra/db/repositories/tests"
-	"github.com/WilliamKSilva/book-reservation/internal/services"
 	"github.com/WilliamKSilva/book-reservation/internal/services/DTOs"
 )
 
@@ -84,60 +83,4 @@ func (uuidService *MockedUuidService) Generate() string {
 
 func NewMockedUuidService() *MockedUuidService {
 	return &MockedUuidService{}
-}
-
-type MockedUserServiceSuccess struct {
-	UserRepository services.UserRepositoryAdapter
-	UuidGenerator  services.UuidServiceAdapter
-}
-
-func NewMockedUserServiceSuccess() *MockedUserServiceSuccess {
-	return &MockedUserServiceSuccess{
-		UserRepository: NewMockedUserRepositorySuccess(),
-		UuidGenerator:  NewMockedUuidService(),
-	}
-}
-
-func (userService *MockedUserServiceSuccess) Create(req DTOs.CreateUserRequestDTO) (DTOs.CreateUserResponseDTO, error) {
-	u, _ := repositories_tests.MockUser()
-	return DTOs.CreateUserResponseDTO{
-		ID:        u.ID,
-		Name:      u.Name,
-		Email:     u.Email,
-		Password:  u.Password,
-		CPF:       u.CPF,
-		BirthDate: u.BirthDate.String(),
-	}, nil
-}
-
-func (userService *MockedUserServiceSuccess) FindByEmail(email string) (DTOs.FindUserByEmailResponseDTO, error) {
-	u, _ := repositories_tests.MockUser()
-	return DTOs.FindUserByEmailResponseDTO{
-		ID:        u.ID,
-		Name:      u.Name,
-		Email:     u.Email,
-		Password:  u.Password,
-		CPF:       u.CPF,
-		BirthDate: u.BirthDate.String(),
-	}, nil
-}
-
-type MockedUserServiceFailure struct {
-	UserRepository services.UserRepositoryAdapter
-	UuidGenerator  services.UuidServiceAdapter
-}
-
-func NewMockedUserServiceFailure() *MockedUserServiceFailure {
-	return &MockedUserServiceFailure{
-		UserRepository: NewMockedUserRepositorySuccess(),
-		UuidGenerator:  NewMockedUuidService(),
-	}
-}
-
-func (userService *MockedUserServiceFailure) Create(req DTOs.CreateUserRequestDTO) (DTOs.CreateUserResponseDTO, error) {
-	return DTOs.CreateUserResponseDTO{}, errors.New("generic MockedUserServiceFailure error")
-}
-
-func (userService *MockedUserServiceFailure) FindByEmail(email string) (DTOs.FindUserByEmailResponseDTO, error) {
-	return DTOs.FindUserByEmailResponseDTO{}, errors.New("generic MockedUserServiceFailure error")
 }
